bec9decbb973f344910c289f33914cd0373cc1a6
KOEGlike <marcell.balazs@yahoo.com>
2023-09-05 14:38:37 +0200

    finished AsyncHandler class

--- src/classes/async_handler.cpp
+++ src/classes/async_handler.cpp
@@ -2,15 +2,35 @@
 
 void AsyncHandler::deleteCallback(uint32_t id)
 {
-    
+    calllbacks.erase(id);
 }
 
-void AsyncHandler::registerCallback(uint32_t id, uint8_t type, std::function<void()> callback)
+uint32_t AsyncHandler::registerCallback(unsigned long delay,uint32_t times, std::function<void()> callback)
 {
-  
+  calllbacks[maxCurrentId]=callback_data{delay,times,callback};
+  return maxCurrentId++;
 }
+
 
 void AsyncHandler::check()
 {
-
+  std::map<uint32_t,callback_data>::iterator it =calllbacks.begin();
+  while (it != calllbacks.end())
+  {
+    if(it->second.timeToTillNextCall<=0)
+    {
+      it->second.callback();
+      it->second.timeToTillNextCall=it->second.delay;
+      it->second.timesCalled++;
+    }
+    if(it->second.times>0)
+    {
+      it->second.timesCalled++;
+    }
+    if(it->second.timesCalled>=it->second.times)
+    {
+      deleteCallback(it->first);
+    }
+    ++it;
+  }
 }
--- src/classes/async_handler.hpp
+++ src/classes/async_handler.hpp
@@ -5,13 +5,26 @@
 #include <inttypes.h>
 #include<map>
 
+struct callback_data
+{
+  callback_data(long delayInMillis,uint32_t timesToRepeat, std::function<void(void)> callBack){
+    delay=delayInMillis;
+    times=timesToRepeat;
+    callback=callBack;
+  }
+  unsigned long delay, timeToTillNextCall=0;
+  int32_t times, timesCalled=0;
+  std::function<void(void)> callback;
+};
+
 class AsyncHandler{
   public:
-    void registerCallback(uint32_t id, uint8_t type, std::function<void(void)> callback);
+    uint32_t registerCallback(unsigned long delay,uint32_t times, std::function<void(void)> callback);
     void deleteCallback(uint32_t id);
     void check();
   private:
-    std::map<uint32_t,std::pair<uint8_t, std::function<void(void)>>> calllbacks;
+    std::map<uint32_t,callback_data> calllbacks;
+    uint32_t maxCurrentId=0;
 };
 
 #endif
